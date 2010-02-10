a37c3c44c16121abe1b836efbfc9fc5fbaf864db
PavelT <pavelt@pavelt-desktop.(none)>
2010-02-10 11:36:34 +0200

    Closes #3 :next_label and :previous_label may be set to nil, false or empty string in order to prevent links to be rendered

--- lib/will_paginate/view_helpers.rb
+++ lib/will_paginate/view_helpers.rb
@@ -234,8 +234,8 @@ module WillPaginate
     def to_html
       links = @options[:page_links] ? windowed_links : []
       # previous/next buttons
-      links.unshift page_link_or_span(@collection.previous_page, 'disabled prev_page', @options[:previous_label])
-      links.push    page_link_or_span(@collection.next_page,     'disabled next_page', @options[:next_label])
+      links.unshift page_link_or_span(@collection.previous_page, 'disabled prev_page', @options[:previous_label]) unless @options[:previous_label].blank?
+      links.push    page_link_or_span(@collection.next_page,     'disabled next_page', @options[:next_label]) unless @options[:next_label].blank?
       
       html = links.join(@options[:separator])
       @options[:container] ? @template.content_tag(:div, html, html_attributes) : html
